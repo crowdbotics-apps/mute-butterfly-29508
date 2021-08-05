98fb6ad6ca20f525e041ce9a88b97ccf06a19a61
Crowdbotics <team@crowdbotics.com>
2021-08-05 14:09:14 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "mute_butterfly_29508Identifier",
+  url: "https://mute_butterfly_29508Identifier.botics.co",
+  api: "https://mute_butterfly_29508Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
